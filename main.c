30707aead1ba955c64ec286efddf6faa0b52d131
Yunus-Yurdagul <160735006+Yunus-Yurdagul@users.noreply.github.com>
2024-05-20 11:43:37 +0300

    Add files via upload
    math.h is header file, main.c and math.c are source file.

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "math.h"
+
+
+int main(void){
+
+	printf("add : %d\n",math_add(3,2));
+
+	printf("multiple : %d\n",math_mul(3 , 2));
+
+	printf("subtract : %d\n",math_sub(3 , 2));
+
+	printf("divide : %d\n",math_div(3 , 2));
+	return 0;
+}
+
